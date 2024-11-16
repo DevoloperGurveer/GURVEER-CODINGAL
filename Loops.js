cbbab27c43a7815100a0174af0833faa926db71d
DevoloperGurveer <gurveercodingal14@gmail.com>
2024-11-16 21:42:48 +0530

    Add files via upload

---
+++ Loops.js
@@ -0,0 +1,45 @@
+< !DOCTYPE html >
+
+    <html>
+
+
+        <head>
+
+            <title></title>
+
+        </head>
+
+        <body>
+        <h2>Javascript for Loop</h2>
+        <p id="demo"></p>
+<script>
+var food = ["Butter Chicken", "Pizza", "Burger", "Pasta", "Rajma Rice"]
+var text="";
+var i;
+for (i=0; i < food.length; i++){text+=fruits[i]+"<br>" }
+document.getElementById("demo").innerHTML=text;
+</script>
+
+
+
+
+
+
+
+
+
+
+
+            <h1></h1>
+
+
+
+
+
+
+
+
+
+        </body>
+
+    </html>
